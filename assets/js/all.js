3e977369c088104018943ca9ba78ca7366f99c2d
Cass K.M. Lam <cass.lam@westcom.com.hk>
2022-08-23 13:45:28 +0800

    Update 2022-08-23T05:45:20.469Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,19 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+// import
+// import AOS from 'aos';
+// DOM
+var wraper = document.querySelector('#wraper'); // imagesLoaded( document.querySelector('#wraper'), function( instance ) {
+//   console.log('all images are loaded');
+// });
+
+imagesLoaded(wraper, function (instance) {// console.log(instance);
+}).on('always', function (instance) {
+  console.log('ALWAYS - all images have been loaded');
+}).on('done', function (instance) {
+  console.log('DONE  - all images have been successfully loaded');
+  AOS.init();
+}).on('fail', function (instance) {
+  console.log('FAIL - all images loaded, at least one is broken');
 });
 //# sourceMappingURL=all.js.map
