475f0f0212fef17c96a712521c38d104f2d6a938
Cass K.M. Lam <cass.lam@westcom.com.hk>
2022-08-23 13:44:31 +0800

    added AOS effect

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,20 @@
-console.log('Hello!');
+// import
+// import AOS from 'aos';
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+// DOM
+const wraper = document.querySelector('#wraper');
+
+// imagesLoaded( document.querySelector('#wraper'), function( instance ) {
+//   console.log('all images are loaded');
+// });
+imagesLoaded(wraper, (instance) => {
+  // console.log(instance);
+}).on( 'always', function( instance ) {
+  console.log('ALWAYS - all images have been loaded');
+}).on( 'done', function( instance ) {
+  console.log('DONE  - all images have been successfully loaded');
+  AOS.init();
+}).on( 'fail', function( instance ) {
+  console.log('FAIL - all images loaded, at least one is broken');
 });
+
